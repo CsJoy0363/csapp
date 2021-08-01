8cc066cae6e212da278002880179b68f7c5319f1
CsJoy0363 <latestworld9041@gmail.com>
2021-08-01 23:14:30 +0600

    initial commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Welcome To The App
+        </p>
+        <a
+          className="App-link"
+          href="https://github.com/CsJoy0363"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Contact
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
